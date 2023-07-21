6c011e8d97c1ff1256e98953c447b3bb89db582f
Maqsood Ahmad <maqsoodmanzoor@gmail.com>
2023-07-21 13:09:21 +0500

    Updates

---
+++ precache-manifest.adb617d0fcf4aa2a579c540193f74e89.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "7758209e8bd865128c92",
+    "url": "//resturant-MUI/static/css/main.c1e659d2.chunk.css"
+  },
+  {
+    "revision": "7758209e8bd865128c92",
+    "url": "//resturant-MUI/static/js/main.f3e32249.chunk.js"
+  },
+  {
+    "revision": "4ed41433b53f4ff89e9a",
+    "url": "//resturant-MUI/static/js/runtime~main.8d08f799.js"
+  },
+  {
+    "revision": "d81684d3c3c7f6007409",
+    "url": "//resturant-MUI/static/js/2.67b766ef.chunk.js"
+  },
+  {
+    "revision": "8a2d3035315010b6dac7",
+    "url": "//resturant-MUI/static/js/3.f9b54713.chunk.js"
+  },
+  {
+    "revision": "5f8fc5fac6ecb7306f6ce746585e30d3",
+    "url": "//resturant-MUI/static/media/logo.5f8fc5fa.svg"
+  },
+  {
+    "revision": "42f6eefe08cac5f773b93e9bc6059d79",
+    "url": "//resturant-MUI/static/media/banner.42f6eefe.jpeg"
+  },
+  {
+    "revision": "3899968c94580d80827f30db921b7cd3",
+    "url": "//resturant-MUI/static/media/dosa.3899968c.jpg"
+  },
+  {
+    "revision": "13f93a5248b638d64a19afc1f0ab3551",
+    "url": "//resturant-MUI/static/media/chhola.13f93a52.jpg"
+  },
+  {
+    "revision": "92eae66d8dc77bff33164bd5a3bab11d",
+    "url": "//resturant-MUI/static/media/idli.92eae66d.jpg"
+  },
+  {
+    "revision": "eb9571da291bd5024cb7b69ac20e968a",
+    "url": "//resturant-MUI/static/media/masala.eb9571da.jpg"
+  },
+  {
+    "revision": "60816264b0adb72c8de180840370130f",
+    "url": "//resturant-MUI/static/media/paneer.60816264.jpg"
+  },
+  {
+    "revision": "533e20e65f7c8e549bcf3ff7428f748f",
+    "url": "//resturant-MUI/static/media/gujrati.533e20e6.jpeg"
+  },
+  {
+    "revision": "86e08ef7d4c6901ae287da6d6fec0792",
+    "url": "//resturant-MUI/index.html"
+  }
+];
